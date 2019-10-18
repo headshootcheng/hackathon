07145c4ff8d0b1aae60bbbaef0498156d833e88f
Peter Cheng <petercheng7788@gmail.com>
2019-10-18 18:37:55 +0800

    no message

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React,{Component} from "react";
 import { View, Text } from "react-native";
 import { createAppContainer  } from "react-navigation";
 import { createStackNavigator } from 'react-navigation-stack'
-import Discovery from './src/discovery'
-import ShoppingCart from './src/shoppingcart'
+import Discovery from './src/society/discovery'
+import ShoppingCart from './src/society/shoppingcart'
 const Allpages = createStackNavigator({
   Discovery:Discovery,
   ShoppingCart:ShoppingCart

051e1f782e812693f13ad07093f713128fce233d
jackchim <jackchim1998@gmail.com>
2019-10-19 02:55:10 +0800

    data update

--- data/data.js
+++ data/data.js
@@ -1,6 +1,7 @@
 export const joined=[
     {
       "_id": 6,
+      
       "Host": "豁 - 國際經濟學商學學生會理大分會 AIESEC",
       "Category": "Event",
       "Nature": "Public",
@@ -68,3 +69,22 @@ export const joined=[
       "Quota (if applicable)": null
     },
   ]
+  export const API = {
+    "APPID":"4948660177",
+    "Sandbox":"false",
+    "WEB_APP":{
+      "MethodId":"4269911701",
+      "APIKEY":"sZC7ctGZZUy+Nzr8cG1grlIvF0Y7lWSVsNOIvBhdsFjYaiX1rpEILV/IzYVsLMp9zDDK7EG5/dlzPRUTPeCdag==",
+      "PublicKey":"MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAwOb7KE6Qy8fHO79A0diSITWSNHi0WAKlEFaZblo2FU9UXeOIdLJ4EIwGbETFHmaqHiRNouGD4pwGtiBM3JhNl24hl3x4kUs9ZGZCHTJAzsFH78Y6ESYPVWiziz1mHFjH9yy0DF2ir9+fHGpOdqJ7rQzNJvOvNyOjIw6wfuNZwQXw76Wyv3rtVGnwGZhb8SiC1pJYk6t5Z2N4BMkUaGSU/kE7kkolkX7VzmsDaPFapcrT4gsx6/rXtt4NeXxpxT6E12mEQc65CN4Uxn2DWDYGoI45YYUwc50FUHYNmghdTJ3/4ex40YBO0SxSQgohhqborWTbP4biPNU3MB987Z9FYScsNcxAaUB69UmfwB8O/kiQ0zejyNIsf2KqNAUBVzpJWqVqvRDVmdrLE0sSFPyLYfzXIpelbpZD2jtXdrldZ640AssmpFvRZxpwrzvL9YUpetnw0KODmxZqcsJqNMtHVi+6DwyMyJjM1fAINbw0kEQ99wO7AzHqrunJa14CX+fgH2MoWdDeuOI84LAcp50eflnfoAsDVREIPEUP4hfvbzpwG8jEm4TfgaNzKDPrqbyHd+4J3ckxEWhB5iKC2pUA4TkVo7EXgD1S/F5kL2wU5d9PKmw0XjWJn+dH02kU0hlCgs1kAl9Npw5FKWtxl11xDCsj+5REnJ0N/wEPlmwgG3cCAwEAAQ=="
+    },
+    "BILL_QR":{
+      "MethodId":"4261121071",
+      "APIKEY":"4T91YKXlUU5Dsp6hkbIQCvPzGjTWQMKsRm5J6xniUiLHMpjlgOzZSSoL5//aDVjDil4TkM3iEqD5v8O/U517Cw==",
+      "PublicKey":"MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAxapzxQsE4z5Gi2EQO4yjBRsuB23gj79jIE5hANur07crWVTRQEIDOiWklez/JAcT/fCR5+30Xp6z83mn0uKroSlt4WrJqq2eRtX6VcS3WSV/1U8zDszokhQaQzsdT47+4bjk8Ead3wpqmbY1O5wT+eZ5BKflrjSTij3YIQGShfIpfdKD0TcTkh5JdFuQ89PoNjhBirkMdhBU7zHVg2genCvXSxHTkOpJocY/z+DwF6gyQ/v6DoaZ26vvPHbvYVBGz1jkVDNFQTXWoC7b/45LdN2dXyl0gUdBUDd9EaZXJz8EZSVmlYk8sEbIdOFenkcEbqhG/ohxnpCEp1ByiDI2uKViW5DHCwddiI4tC/3s5eVMMrPffbswEioKX6UCGuptpGBWrqvhkXBxBDIGqaFfdqAHLAGhVFw8bxFz/oIBcVouDLupx8RqBm5S1FE9pNx0Cl1SQsyqrahqNX3rFIu4grLYC1y8+6ZQUPTEF0BaKBYZZ815t0xImKwJrC3murDfFKhkOma0upbqFKJqGXcMKPBH+gxmGR05anPYKrXIKwOENgCBnJzrc6pOHS5JZcwuoYTNrqanZPaUZE9t8fLZYPJQmA7jgAz6mgfmi2f2Ko1i6DygoProxr1dwR9weUugJzMLoS+1g0AJI0FiqxuvQ4TyKuXdUWDk8NL/8ffGaHcCAwEAAQ=="
+    },
+    "BILL_QR":{
+      "MethodId":"4270229638",
+      "APIKEY":"2anSfuoe96Z3k4MkUItGrhQbdt5DMXGkviaTM1L3InfrNjhADQ0GgQlXrId+BFGm8gMAaIKwl2PvMVLzVU+wjw==",
+      "PublicKey":"MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAvcfp6mH7s4xDvbya6i3S9k/AI4xQJwcsGBGPdd9X2m5PbFxX5LTsDzlHqQb873IT7AtpotrtO16rfyGyA8AR+fOZ97ARx6zOvAgCmeKaOXMp7miySy4Kzqzd+cf79qHYvtL1T9tuNuv5JSeIfmH2DbG8Vjd882PupdksDJReTFLWqTIcgbh75CRqWCj8yKqJ17NSS/212KwKbE81InMOydQVtx8eAv5z40270GkOWJXqsCkUiJzxmym+4kJFzOnsILW2s2K0T//ufpKzXes+7ZZOIzeQBRA2MZ4KUssyIIG7MXDegWUOoX/1DtAponE+ibuMA4Ve7NFJPBkXSoaL4heWh7PwYti/98E4fPWTTysXm44a1WVgZ2nqXYxWgR/TbEdwPF4NhawqCNJwfHgseqGRrE8s89ok6d0kCPO+dOvPP/3F6z01HgTIFVKO0NNcGKHrwsLMeqSa9PG35PzKzQ0XUREUmLSj2QQg2+pMn9s9FWigAnN5KBzieJqRKLFURnC4wvqCzwZUMUCv3W0AMo7278zdNym9wAqwBp3w4ay3QmvKdRfOK9l3/HAUlvkUXhjTMyY3zygDZKsYjHj0F1IPrRnVDC/w3t3TG4oUe8Pp+5kyKnsas9ZaatEYL6mifos0kkutpMwUJxVhwULvucqklJTWVBHCn9jVhG7e2U0CAwEAAQ=="
+    },
+  }
--- src/society/news.js
+++ src/society/news.js
@@ -1,11 +1,14 @@
 import React,{Component} from "react";
-import { View, Text, ScrollView,Image,TouchableOpacity,Linking,Platform,ImageBackground } from "react-native";
+import { View, Text, ScrollView,Image,TouchableOpacity,Linking,Platform,ImageBackground,NativeEventEmitter } from "react-native";
 import {Container,Header,Title,Content,Footer,FooterTab} from 'native-base';
 //import { CheckBox } from 'react-native-elements'
 import CustomHeader from '../common/customHeader';
 import CustomFooter from '../common/customFooter';
 import { Actions } from 'react-native-router-flux';
+import {RNTapNGoSDK, TapNGoSDK} from './RNTapNGoSDKClass';
+import {API} from '../../data/data'
 let styles=require('../../stylesheet/style');
+var subscription;
 export default class News extends React.Component{
     static navigationOptions = {
         header:null
@@ -13,23 +16,83 @@ export default class News extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            
+            merTradeNo:"TEST20191019",
+            price:null,
+            currency:null,
+            remark:"",
+            notifyUrl:"",
         };
       }
-      componentDidMount(){
-
+      singlepayment(item){
+          console.log(item)
+          let merTradeNo = "TEST20191019"
+          let price = parseFloat(item.price)
+          let currency = "HKD"
+          let remark = item.Host + item.Product
+          let notifyUrl = ""
+        RNTapNGoSDK.doSinglePayment(API.Sandbox,
+            API.APPID,
+            API.WEB_APP.APIKEY,
+            API.WEB_APP.PublicKey,
+            API.APPID,
+            merTradeNo,
+            price,
+            currency,
+            remark,
+            notifyUrl
+           );
       }
+      async getMoviesFromApi() {
+        try {
+        //   let response = await fetch(
+        //     'https://datastudio-api.hkstp.org:443/CPhackathon2019/v1.0.0/datastore_search?resource_id=dabe12a3-875d-47d2-b11b-294c1311bffc'
+        //     ,{
+        //         method: 'GET',
+        //         headers: {
+        //           Accept: 'application/json',
+        //           'Authorization': 'Bearer 72eebdaeb9b302ac55dd9a36d73b6ba0',
+        //         }})
+        let response = await fetch(
+            'https://facebook.github.io/react-native/movies.json'
+            )
+          let responseJson = await response.json();
+          return responseJson;
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      getdata(){
+        this.getMoviesFromApi()
+        .then(
+            (response) => console.log(response)
+        )
+      }
+      componentDidMount(){
+       const paymentResultEmitter = new NativeEventEmitter(TapNGoSDK);
+       subscription = paymentResultEmitter.addListener("TGSDKPayResult", (result) => {
+       // Handle the result
+       console.log(result)
+       });
+      } 
+      componentWillUnmount(){
+            subscription.remove();
+        }
+      
     render=()=>{
         const { state } = this.props.navigation
         var joined_raw = state.params.joined
+        var react = this
         var joined = joined_raw.map(function(item) {
-            return (<ImageBackground source={require('../../image/featured-event02.jpg')} style={styles.eachfeatureeventarea}>
+            // return (<TouchableOpacity  onPress={()=>{react.singlepayment(item)}} >
+            return (<TouchableOpacity  onPress={()=>{react.getdata()}} >
+           <ImageBackground source={require('../../image/featured-event02.jpg')} style={styles.eachfeatureeventarea}>
                     <Text style={styles.eachfeatureeventtext}>{item.Product}</Text>
                     <View style={{marginTop:20,alignItems:"center",justifyContent:"center"}}>
                         <Text style={styles.eachfeatureeventtext2}>{item.Host}</Text>
                         <Text style={styles.eachfeatureeventtext2}>{item.Period}</Text>
                     </View>
-                </ImageBackground>)
+                </ImageBackground>
+                </TouchableOpacity>)
         });
         var record_raw = state.params.record
         var record = record_raw.map(function(item) {
